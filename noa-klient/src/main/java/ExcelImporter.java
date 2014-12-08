ebe309687aaeebf5101908a8a5353a2806562a55
Arne-Richard Hofsøy <arne-richard.hofsoy@bouvet.no>
2014-12-08 15:33:20 +0100

    First draft Importer for excel files

--- noa-klient/src/main/java/ExcelImporter.java
+++ noa-klient/src/main/java/ExcelImporter.java
@@ -1,10 +1,13 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -31,6 +34,7 @@ public class ExcelImporter extends Application {
 
     Button selectFiles;
     Button importFiles;
+    Button clearForm;
     Stage primaryStage;
     List<File> fileList;
     BorderPane bp = null;
@@ -57,15 +61,28 @@ public class ExcelImporter extends Application {
                 });
         importFiles = new Button("Importer filer");
         importFiles.setDisable(true);
+        clearForm = new Button("Reset");
+
+        clearForm.setOnAction(event -> {
+            fileList.clear();
+            importFiles.setDisable(true);
+        });
 
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(selectFiles, importFiles);
+        vBox.getChildren().addAll(selectFiles);
         bp.setTop(selectFiles);
-        bp.setBottom(importFiles);
+        HBox bottomBox = new HBox();
+        bottomBox.getChildren().addAll(importFiles, clearForm);
+        bp.setBottom(bottomBox);
+
 
         root.getChildren().addAll(bp);
 
         ScrollPane s1 = new ScrollPane();
+        s1.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+
+        s1.setVmax(440);
+        s1.setPrefSize(750, 440);
         this.vBox = new VBox();
         s1.setContent(this.vBox);
         bp.setCenter(s1);
@@ -83,8 +100,8 @@ public class ExcelImporter extends Application {
     private void updateVbox(List<File> files) {
         for (File f : files) {
             Label file = new Label(f.getName());
-            Button delete = new Button("delete");
-            this.vBox.getChildren().addAll(file, delete);
+            //Button delete = new Button("delete");
+            this.vBox.getChildren().addAll(file);
         }
         this.fileList.addAll(files);
     }
