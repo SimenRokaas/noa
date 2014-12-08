eff4a6e20034241de6582ee5aa6dc1b4e0f25eff
Arne-Richard Hofsøy <arne-richard.hofsoy@bouvet.no>
2014-12-08 13:51:30 +0100

    Lagt til spring boot

---
+++ noa-grensesnitt/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by arne-richard.hofsoy on 08/12/14.
+ */
+
+@Controller
+@EnableAutoConfiguration
+public class Main extends Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
