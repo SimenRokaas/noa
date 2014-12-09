5d0f7fff7d1f10ae0ad0ef92fa8963e43f72bf40
Arne-Richard Hofsøy <arne-richard.hofsoy@bouvet.no>
2014-12-09 12:25:16 +0100

    Fest assert, junit jars i pom.xml.

--- noa-klient/src/test/java/no/statnett/noa/excel/ImportererTest.java
+++ noa-klient/src/test/java/no/statnett/noa/excel/ImportererTest.java
@@ -1,6 +1,18 @@
 package no.statnett.noa.excel;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
 
 public class ImportererTest {
+
+    @Test
+    @Ignore
+    public void testAtFilKanLesesInn() {
+        assertThat(false).isTrue();
+
+    }
+
 
 }
